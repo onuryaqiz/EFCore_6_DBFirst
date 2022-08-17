27da32e8ad615a57ce13ccd13db80e1a5e32c058
Onur Yağız <95749456+onuryaqiz@users.noreply.github.com>
2022-08-17 18:16:25 +0300

    Add project files.

---
+++ UdemyEFCore.DatabaseFirst/DAL/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyEFCore.DatabaseFirst.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; } // Property ile DB'deki tablo ismi aynı ise EFCore bunu eşleştirir.
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) // SQL Server'a bağlantı cümleciği.
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=UdemyEFCoreDatabaseFirstDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+    }
+}
---
+++ UdemyEFCore.DatabaseFirst/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using UdemyEFCore.DatabaseFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
+using (var _context = new AppDbContext()) // Veritabanı ile ilgili işlemleri buradan gerçekleştireceğiz.
+{
+    var products = await _context.Products.ToListAsync(); // Veritabanındaki Product tablosu ile haberleş demektir. 
+
+
+    products.ForEach(p =>
+    {
+
+        Console.WriteLine($"{p.Id}: {p.Name}");
+
+    });
+
+}
