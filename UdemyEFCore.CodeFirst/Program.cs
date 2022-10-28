9e17506f8cba3cd6c6af107b10d21f72ca728c44
Onur Yağız <95749456+onuryaqiz@users.noreply.github.com>
2022-10-28 14:22:28 +0300

    EFCore tarafından track edilen bir entity'de güncelleme yapıldığında update() methodunun çağrılmasına gerek yoktur. Örnek eklendi.

--- UdemyEFCore.CodeFirst/Program.cs
+++ UdemyEFCore.CodeFirst/Program.cs
@@ -7,14 +7,37 @@ Initializer.Build(); // Build methodu ile appsettings.json'u okunabilir hale get
 
 using (var context = new AppDbContext())
 {
-    context.Products.Add(new() { Name = "Kalem 1", Price = 200, Stock = 100, Barcode = 123 });
-    context.Products.Add(new() { Name = "Kalem 1", Price = 200, Stock = 100, Barcode = 123 });
-    context.Products.Add(new() { Name = "Kalem 1", Price = 200, Stock = 100, Barcode = 123 });
 
-    Console.WriteLine($"Context Id:{context.ContextId}"); // Eğer uygulamada birden fazla context instance varsa contextId kullanılabilir.
-   // context.Database
-   // context.SaveChanges();
-  
+
+    var products = context.Products.ToList();
+    products.ForEach(p =>
+    {
+        p.Stock += 100; // burada modified track edildiği için aşağıdaki koda gerek yoktur ! Stock'ları 100 100 artırdık .
+
+        // context.Products.Update(p); buna gerek yok !
+
+    });
+
+   context.ChangeTracker.Entries().ToList().ForEach(e =>
+
+
+    {
+        if (e.Entity is Product product)
+        {
+            Console.WriteLine(e.State);
+        }
+
+    });
+
+
+    //var product = context.Products.First(); // Update methodu için 
+
+    //product.Name = "kalem 100";
+    //product.Price = 200;
+
+    //context.Update(product); Bunu EFCore tarafından track edilmiş bir metod var ise Update yazmaya gerek yoktur. Update methodu EFCore tarafından Track edilmeyen nesnelere yazılabilir.
+    context.SaveChanges();
+
 
 
 }
